cae3617045620fa5213c214620aacf587430a49a
ziyad <ziyadmuhammad9696@gmail.com>
2024-12-20 19:13:07 +0530

    authentication componets added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './components/App.jsx'
-
+import Signup from './components/admin/Signup'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Signup />
   </StrictMode>,
 )

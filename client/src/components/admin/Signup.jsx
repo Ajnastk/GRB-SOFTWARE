fb807646251587f056afd14fe6c7d50e64a94790
Abdul rahib <rahibrahib6740@gmail.com>
2024-12-21 15:32:47 +0530

    route checking

--- client/src/components/admin/Signup.jsx
+++ client/src/components/admin/Signup.jsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-
-import { Link } from "react-router-dom";
-
-
+// import { Link }  from "react-router-dom";
 const Signup = () => {
     const [showPassword, setShowPassword] = useState(false);
     const togglePasswordVisibility = () => {

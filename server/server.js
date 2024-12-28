4c2ed786e3d3f1aeacbce5d2103431d8867c2640
muhammed ajnas tk <ajnastk432@gmail.com>
2024-12-28 11:35:05 +0530

    check

--- server/server.js
+++ server/server.js
@@ -4,17 +4,21 @@ const dotenv=require('dotenv').config();
 const cors = require('cors');
 const AuthRoute=require('./routes/authRoutes');
 const reviewRoutes = require('./routes/ReviewRoute') 
-const {notFound,errorHandler }=require('./middlewares/errorhandler')
+const { notFound, errorHandler } = require('./middlewares/errorhandler');
 
 const app=express();
 const PORT=process.env.PORT || 5000;
 const MONGO_URL=process.env.MONGO_URI;
 
 app.use(cors());
-app.use(notFound,errorHandler);
+
 app.use(express.json());
-app.use('/api',AuthRoute);
-app.use('/api',reviewRoutes);
+app.use('/api', AuthRoute);
+app.use('/api', reviewRoutes);
+
+// Error Handling Middleware (should be last)
+app.use(notFound);
+app.use(errorHandler);
 
 
 mongoose.connect(MONGO_URL)

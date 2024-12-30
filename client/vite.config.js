730c42ff597fd4be41d6627f1724858380900eed
ziyad <ziyadmuhammad9696@gmail.com>
2024-12-30 11:00:11 +0530

    conflicts resolved 0.1

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,17 +4,7 @@ import envCompatible from 'vite-plugin-env-compatible'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-=======
+
   envPrefix: "REACT_APP_",
   plugins: [react(),envCompatible()],
   server: {
@@ -23,6 +13,5 @@ export default defineConfig({
   },
    build: {
     outDir: 'build', // Specify the output directory for the build
->>>>>>> e4c8242f518cb722d5311dc5d71f82d378751978
   },
 })

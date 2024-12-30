8a05cffa14c02f07c6c567d80429944f7e47c6ec
ziyad <ziyadmuhammad9696@gmail.com>
2024-12-30 10:07:29 +0530

    test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,39 +1,23 @@
-
-
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/index.css";
 import Signup from "./components/admin/Signup.jsx";
 import Login from "./components/admin/Login.jsx";
- import Rating from "./components/user/Rating.jsx";
-import AdminPage from "./components/admin/AdminPage.jsx" 
+import Rating from "./components/user/Rating.jsx";
+import AdminPage from "./components/admin/AdminPage.jsx";
 
 createRoot(document.getElementById("root")).render(
-
-
-     
-
-
-
   <BrowserRouter>
     <Routes>
-    <Route path="/review-list" element={ <AdminPage/>}/>
-    <Route path="/rating/:adminId" element={<Rating />} />
-
+      <Route path="/review-list" element={<AdminPage />} />
+      <Route path="/rating/:adminId" element={<Rating />} />
 
       <Route path="/" element={<Signup />} />
-      <Route path="/login" element={<Login/>} />
-     {/* <Route path="/rating" element={<Rating />} />  */}
+      <Route path="/login" element={<Login />} />
+      {/* <Route path="/rating" element={<Rating />} />  */}
     </Routes>
-    
   </BrowserRouter>
-)
-
-
-
-
-    
-
+);
 
 // import ReactDOM from "react-dom/client";
 // import { createBrowserRouter, RouterProvider} from "react-router-dom";

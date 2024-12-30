bd0a2f5e392de921e18f04d179638541f8661cab
muhammed ajnas tk <164134564+Ajnastk@users.noreply.github.com>
2024-12-30 17:23:14 +0530

    Update Rating.jsx

--- client/src/components/user/Rating.jsx
+++ client/src/components/user/Rating.jsx
@@ -20,7 +20,7 @@ const Rating = () => {
       rating: selectedRating,
       description: textInput.trim(),
     };
-    const backendUrl = `${import.meta.env.REACT_APP_BACKEND_URL}/`;
+    const backendUrl = `${import.meta.env.REACT_APP_BACKEND_URL}`;
     const token= localStorage.getItem('token');
     console.log('Token from local storage',token)
       if(!token){

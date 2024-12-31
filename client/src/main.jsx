4452b48e4666700b26f0d72719ef4863da51548d
muhammed ajnas tk <ajnastk432@gmail.com>
2024-12-31 12:06:41 +0530

    admin Id

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,13 +7,15 @@ import Signup from "./components/admin/Signup.jsx";
 import Login from "./components/admin/Login.jsx";
  import Rating from "./components/user/Rating.jsx";
 import AdminPage from "./components/admin/AdminPage.jsx" 
+import Home from "./components/admin/Home.jsx"
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
+      <Route path="/" element ={<Home/>}/>
     <Route path="/review-list" element={ <AdminPage/>}/>
     <Route path="/rating/:adminId" element={<Rating />} />
-      <Route path="/" element={<Signup />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login/>} />
     </Routes>
     

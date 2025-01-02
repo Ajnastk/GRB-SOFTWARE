bc7b08675cea324ce38e1189ef9df1d03ccaf311
muhammed ajnas tk <ajnastk432@gmail.com>
2025-01-02 18:08:13 +0530

    dj

--- client/src/components/admin/AdminPage.jsx
+++ client/src/components/admin/AdminPage.jsx
@@ -5,6 +5,7 @@ import ReviewList from "./ReviewList";
 
 const AdminPage = () => {
   const { adminId }=useParams();
+  
   return (
     <div className="min-h-screen bg-slate-50 p-8">
       <h1 className="text-3xl font-bold text-center mb-8">Admin Dashboard</h1>

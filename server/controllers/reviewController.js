8b13b332d0ffcfc6707bd0bf888941643e4f8780
Abdul rahib <rahibrahib6740@gmail.com>
2025-01-02 17:45:03 +0530

    rating page setup

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -3,13 +3,14 @@ const AdminModel = require("../model/AdminSchema"); // Assuming you have the Adm
 
 const forDescription = async (req, res) => {
   try {
-    const { rating, description } = req.body;
+    const { rating, description} = req.body;
 
-    const adminId= req.admin?.adminId;
+    const adminId =req.params.adminId;
+
     console.log('AdminId is',adminId);
+
     if(!adminId){
-      console.error('AdminId is not provided');
-      return res.status(400).json({error:'Invalid admincredention'});
+      return res.status(400).json({error:'AdminId is not found'});
     }
 
     if (rating <= 3) {
@@ -35,15 +36,15 @@ const forDescription = async (req, res) => {
       return res.status(200).json("Review submitted successfully.");
     } else if (rating >= 4) {
       // Redirect to the Google review link for ratings 4 or above
-      const adminData = await AdminModel.findOne(); // Assuming there's one admin record in your database
-
+      const adminData = await AdminModel.findOne({_id:adminId}); // Assuming there's one admin record in your database
+      console.log('admin data is',adminData);
       if (!adminData || !adminData.googlelink) {
         console.error("Admin Google review link is missing.");
         return res.status(500).json({
           error: "Google review link is not configured.",
         });
       }
-
+      console.log('redirect url is this:',adminData.googlelink);
       // Respond with the Google link for front-end redirection
       return res.status(200).json({
         redirectUrl: adminData.googlelink,
@@ -60,13 +61,13 @@ const forDescription = async (req, res) => {
 
 const getReviews = async (req, res) => {
   try {
-    const adminId = req.admin?.adminId;
+    const adminId =req.params.adminId;
 
     console.log('admin id is ',adminId)
      if (!adminId) {
-      return res.status(400).json({ error: "Invalid admin credention" });
+      return res.status(400).json({ error: "Admin Id is required" });
     }
-    const reviews = await ReviewModel.find({adminId:req.admin.adminId }); // Fetch all reviews from the database
+    const reviews = await ReviewModel.find({adminId}); // Fetch all reviews from the database
     console.log("Reviews fetched for admin:", reviews);
     
     res.status(200).json(reviews); // Return the reviews as JSON

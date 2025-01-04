3e0d5b42343d0503aefbb4ba34b675abb5f7b4d4
muhammed ajnas tk <ajnastk432@gmail.com>
2025-01-04 17:09:39 +0530

    ui updates

--- client/src/components/admin/ReviewList.jsx
+++ client/src/components/admin/ReviewList.jsx
@@ -80,7 +80,7 @@ const ReviewList = () => {
                     )}
                   </div>
                 </div>
-                <p>{review.description}</p> {/* Handle missing comment */}
+                <p className="text-lg from-neutral-800 text-gray-600" >{review.description}</p> {/* Handle missing comment */}
                 <p className="text-sm text-gray-500 mt-2">
                   Date:{" "}
                   {review.createdAt

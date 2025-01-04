ab5e26137f327623ee7a9ee03865e1255c04a84d
muhammed ajnas tk <ajnastk432@gmail.com>
2025-01-04 12:05:05 +0530

    fix

--- server/routes/ReviewRoute.js
+++ server/routes/ReviewRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const {forDescription , getReviews } = require ('../controllers/reviewController') 
-const { authMiddlware } =require('../middlewares/authmiddlware')
+const { authMiddleware } =require('../middlewares/authmiddlware')
 const router = express.Router();
 
 router.post('/review-submit/:adminId',forDescription);
-router.get('/review',getReviews)
+router.get('/review',authMiddleware,getReviews)
 
 
 module.exports = router;

6b27e8a69fa3722086cc5e483b41722bdde0f703
muhammed ajnas tk <ajnastk432@gmail.com>
2025-07-11 12:37:51 +0530

    fix build error

--- grb-software/app/admin/login/page.js
+++ grb-software/app/admin/login/page.js
@@ -28,7 +28,7 @@ export default function LoginPage() {
     try {
 
       const apiUrl = 
-        process.env.NODE_ENV = 'development'
+        process.env.NODE_ENV === 'development'
         ? process.env.NEXT_PUBLIC_API_URL_DEV
         : process.env.NEXT_PUBLIC_API_URL_PROD;
 
--- grb-software/app/api/admin-signup/route.js
+++ grb-software/app/api/admin-signup/route.js
@@ -75,7 +75,7 @@ export async function POST(req) {
     // Generate QR code buffer
 
      const apiUrl = 
-        process.env.NODE_ENV = 'development'
+        process.env.NODE_ENV === 'development'
         ? process.env.NEXT_PUBLIC_API_URL_DEV
         : process.env.NEXT_PUBLIC_API_URL_PROD;
 

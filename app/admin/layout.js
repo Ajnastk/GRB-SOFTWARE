819cd3e0c7aa1d2d68a6c27081af0db2e7a74a38
muhammed ajnas tk <ajnastk432@gmail.com>
2025-09-01 11:13:00 +0530

    merge error fix

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,52 +1,6 @@
 "use client";
-
-<<<<<<< HEAD
-import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
-=======
-// import { useState,useEffect } from 'react';
-// import { usePathname } from 'next/navigation';
-// import AdminSidebar from '@/component/AdminSidebar';
-// import AdminHeader from '@/component/AdminHeader';
-
-// export default function AdminLayout({ children }) {
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
-//   const {data : session} = useSession();
-//   const pathname = usePathname();
-
-//   // Don't show sidebar and header on login page
-//   const noLayoutRoutes = ['/admin/login', '/admin/signup', '/admin'];
-//   if (noLayoutRoutes.includes(pathname)) {
-//   return <div className="min-h-screen">{children}</div>;
-// }
-
-//   return (
-//     <div className="min-h-screen bg-gray-50">
-//       {/* Sidebar */}
-//       <AdminSidebar 
-//         isOpen={sidebarOpen} 
-//         onClose={() => setSidebarOpen(false)} 
-//       />
-
-//       {/* Main content wrapper - push content to the right on desktop */}
-//       <div className="lg:pl-64">
-//         {/* Header */}
-//         <AdminHeader onMenuClick={() => setSidebarOpen(true)} />
-
-//         {/* Main content */}
-//         <main className="p-6">
-//           <div className="max-w-7xl mx-auto">
-//             {children}
-//           </div>
-//         </main>
-//       </div>
-//     </div>
-//   );
-// }
-
 import { useState, useEffect } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
->>>>>>> 176546464867a0011bad979690cdc2bac136437b
 import AdminSidebar from '@/component/AdminSidebar';
 import AdminHeader from '@/component/AdminHeader';
 
@@ -65,16 +19,12 @@ export default function AdminLayout({ children }) {
     }
   }, [pathname, router]);
 
-<<<<<<< HEAD
   useEffect(() => {
     fetch('/api/admin/me/route')
       .then(res => res.json())
       .then(data => setAdmin(data));
   }, []);
 
-  // Don't show sidebar and header on login page
-=======
->>>>>>> 176546464867a0011bad979690cdc2bac136437b
   const noLayoutRoutes = ['/admin/login', '/admin/signup', '/admin'];
   if (noLayoutRoutes.includes(pathname)) {
     return <div className="min-h-screen">{children}</div>;
@@ -87,14 +37,10 @@ export default function AdminLayout({ children }) {
         onClose={() => setSidebarOpen(false)}
       />
       <div className="lg:pl-64">
-<<<<<<< HEAD
         {/* Header */}
         <AdminHeader onMenuClick={() => setSidebarOpen(true)} admin={admin} />
 
         {/* Main content */}
-=======
-        <AdminHeader onMenuClick={() => setSidebarOpen(true)} />
->>>>>>> 176546464867a0011bad979690cdc2bac136437b
         <main className="p-6">
           <div className="max-w-7xl mx-auto">{children}</div>
         </main>
